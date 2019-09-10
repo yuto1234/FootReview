b7d3fd4d60b9fd73577f08940ee9c95704ef7ac4
YutoAraki <mjgda85200@yahoo.co.jp>
2019-09-10 20:55:10 +0900

    #14 nameカラムのNOT NULL制約のつけ忘れを修正

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_09_10_112158) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
