c5621c431f678b3688c8824278bb2e84a7443301
YutoAraki <mjgda85200@yahoo.co.jp>
2019-09-11 10:20:54 +0900

    #16 ユーザーモデルの単体テスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+    it "name,email,password,password_confirmationがあればUserを保存できる" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "nameカラムが空の場合登録できない" do
+      user = build(:user, name: "")
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    it "emailカラムが空の場合登録できない" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "passwordが空の場合登録できない" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "password_confirmationが空の場合登録できない" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it "emailが重複していた場合登録できない" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+
+    it "パスワードが５文字以下だと登録できないこと" do
+      user = build(:user, password: "55555", password_confirmation: "55555")
+      user.valid?
+      expect(user.errors[:password][0]).to include("is too short")
+    end
+  end
+end 
