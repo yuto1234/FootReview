eb20221d9e659b7da7bf74823fc24151475d68e6
YutoAraki <mjgda85200@yahoo.co.jp>
2019-09-11 21:31:18 +0900

    #23 試合詳細ページにスコアボードを表示

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,8 @@ class GamesController < ApplicationController
   def index
     @games = Game.order(date: :desc).page(params[:page]).per(5)
   end
+
+  def show
+    @game = Game.find(params[:id])
+  end
 end
