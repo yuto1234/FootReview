1cc474df408799e35f777f4b6d4477a98fb4eaa4
YutoAraki <mjgda85200@yahoo.co.jp>
2019-09-13 09:54:01 +0900

    #28 投稿ページまでのリンクを設置

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,11 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: :new # 未ログインユーザーをログイン画面に遷移
+
   def show
   end
 
   def new
+    @review = Review.new
   end
 
   def create
