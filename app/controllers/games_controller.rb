8fc74dc04a99c1816ea470fd300d49a4f96e942d
YutoAraki <mjgda85200@yahoo.co.jp>
2019-09-21 19:22:17 +0900

    #41 レビュー一覧を表示

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,5 +6,6 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
     @game_members = GameMember.where(game_id: @game)
+    @reviews = Review.where(game_id: @game).includes(:user).order(created_at: :desc).page(params[:page]).per(5)
   end
 end
