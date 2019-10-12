d202d793cf7a09910ab69c4861b04940e981312f
YutoAraki <mjgda85200@yahoo.co.jp>
2019-10-12 13:22:30 +0900

    nameで登録できるようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password, :remember_me])
   end
 end
