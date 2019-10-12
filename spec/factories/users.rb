8e549d57f5291f83297db8dad7d122243f7d6a2c
YutoAraki <mjgda85200@yahoo.co.jp>
2019-10-12 13:38:25 +0900

    Userモデルの単体テストを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     id                    { 1 }
     name                  {"test"}
-    email                 {"test@gmail.com"}
     password              {"666666"}
     password_confirmation {"666666"}
   end
